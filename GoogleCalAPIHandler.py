5ccf08334320f226fb85b4c2162755673544e745
Andy Harrison <iam.andyharrison@gmail.com>
2019-03-17 18:38:30 +0000

    Template for Google Calendar API handler class
    Plus template for the associated unit tests

---
+++ GoogleCalAPIHandler.py
@@ -0,0 +1,8 @@
+
+class GoogleCalAPIHandler:
+    """Class for handling the Google Calendar API: see https://developers.google.com/calendar/quickstart/python"""
+    # members
+
+    # creator methods
+    def __init__(self):
+        pass
---
+++ testGoogleCalAPIHandler.py
@@ -0,0 +1,16 @@
+import unittest
+from GoogleCalAPIHandler import GoogleCalAPIHandler
+
+
+class testXLSXhandler(unittest.TestCase):
+    def SetUp(self):
+        """set-up code, which is called before each test, to avoid repetition"""
+        pass
+
+    def tearDown(self):
+        """code to tidy up after each test"""
+        pass
+
+# run tests
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
