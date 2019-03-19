955d82e927d13d549d5aa68d01b3dd06ed462ec4
Andy Harrison <iam.andyharrison@gmail.com>
2019-03-19 21:21:01 +0000

    Define creator for read-only access to Google calendar API

--- GoogleCalAPIHandler.py
+++ GoogleCalAPIHandler.py
@@ -1,8 +1,40 @@
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
 class GoogleCalAPIHandler:
-    """Class for handling the Google Calendar API: see https://developers.google.com/calendar/quickstart/python"""
+    """Class for handling the Google Calendar API:
+    intro see https://developers.google.com/calendar/quickstart/python (steps 1 and 2 done)
+    add event https://developers.google.com/calendar/create-events"""
     # members
+    # If modifying these scopes, delete the file token.pickle.
+    SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+    creds = None
+    service = None
 
     # creator methods
     def __init__(self):
-        pass
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                self.creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not self.creds or not self.creds.valid:
+            if self.creds and self.creds.expired and self.creds.refresh_token:
+                self.creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', self.SCOPES)
+                self.creds = flow.run_local_server()
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(self.creds, token)
+
+        self.service = build('calendar', 'v3', credentials=self.creds)
+
