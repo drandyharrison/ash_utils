7faf10efff91b67e0cf0ba1394e42b2e7db59731
Andy Harrison <iam.andyharrison@gmail.com>
2019-05-26 22:20:57 +0100

    Create __init__.py to make ash_utils into a package
    My first attempt at a package

---
+++ __init__.py
@@ -0,0 +1,8 @@
+import ash_singleton
+from create_barchart import create_barchart
+from GoogleCalAPIHandler import GoogleCalAPIHandler
+from JSONhandler import JSONhandler
+from replace_nonnumeric import replace_nonnumeric
+from TimeZone import TimeZone
+from URLhandler import URLhandler
+from XLSXhandler import XLSXhandler
